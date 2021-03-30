d6410c3c653229a862658bd0c627e3ad8f6b3fd7
Pedro B <bereilhp@gmail.com>
2021-03-30 20:41:46 +0200

    HashSet

---
+++ HashSetClass.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class HashSetClass {
+
+    public static void main(String[] args) {
+
+        HashSet<String> cars = new HashSet<String>();
+
+            cars.add("Ferrari");
+            cars.add("Volvo");
+            cars.add("BMW");
+            cars.add("Audi");
+            cars.add("Mazda");
+            cars.add("Ford");
+
+            System.out.println(cars);
+
+    }
+}
